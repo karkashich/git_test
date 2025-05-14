24922daef6087c676475e47e78182e9439aa7651
Vyacheslav Shikhirin <vavik146b@yandex.ru>
2025-05-14 20:25:17 +0300

    first commit

---
+++ add_task_noproject_case№1_test.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service  
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+def test_opentaskpage():
+    # Указываем полный путь к chromedriver.exe
+    driver_path = os.path.abspath("webdriver/chromedriver.exe")  # или r"webdriver\chromedriver.exe"
+    service = Service(executable_path=driver_path)
+    driver = webdriver.Chrome(service=service)
+
+    def add_task_noproject_case1(driver):
+        driver.get("https://portal.test.app/#/projects/missions/5")
+        # обязательный блок так как вход не запоминается \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        wait = WebDriverWait(driver, 10)
+        log_in = wait.until(EC.presence_of_element_located((By.ID, "social-keycloak-oidc")))
+        log_in.click()
+        # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        WebDriverWait(driver, 3)
+        #находим наш span и делаем его кликабельным
+        span_text = "Создать задачу"
+        span_element = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, f"//span[text()='{span_text}']"))
+        )
+        span_element.click()
+
+        # 1. Ждем модальное окно
+        modal = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//*[contains(@style, 'z-index') and number(substring-before(substring-after(@style, 'z-index:'), ';')) > 2399]")))
+        
+        # 2. Находим input через `for`
+        input_field = modal.find_element(By.ID, "input-0")
+        
+        # 4. Взаимодействие
+        input_field.click()
+        input_field.send_keys("new autotask case №1")
+
+        span_btn = " Создать "
+        span_task = WebDriverWait(modal, 10).until(
+            EC.element_to_be_clickable((By.XPATH, f".//span[text()='{span_btn}']"))
+        )
+        span_task.click()
+        
+        assert "new autotask case №1" in driver.page_source
+
+    add_task_noproject_case1(driver)
+    driver.quit()
---
+++ task_noproject_case№1_delete_test.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service  
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+def test_deletetask():
+    # Указываем полный путь к chromedriver.exe
+    driver_path = os.path.abspath("webdriver/chromedriver.exe")  # или r"webdriver\chromedriver.exe"
+    service = Service(executable_path=driver_path)
+    driver = webdriver.Chrome(service=service)
+
+    def add_task_noproject_case1(driver):
+        print('nachalo')
+        driver.get("https://portal.test.app/#/projects/missions/5")
+        # обязательный блок так как вход не запоминается \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        wait = WebDriverWait(driver, 10)
+        log_in = wait.until(EC.presence_of_element_located((By.ID, "social-keycloak-oidc")))
+        log_in.click()
+        # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+        driver.get("https://portal.test.app/#/projects/missions/5")
+
+        task_from_case1 = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//a[text()='new autotask case №1']"))
+        )
+
+        task_from_case1 = driver.find_element(By.XPATH, "//a[text()='new autotask case №1']")
+        task_from_case1.click()
+
+        WebDriverWait(driver, 10)
+
+        for window_handle in driver.window_handles:
+            if window_handle != driver:
+                driver.switch_to.window(window_handle)
+                break
+
+
+        delete_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, f".//span[text()='Удалить']"))
+        )
+
+        delete_btn.click()
+
+        delete_btn = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, f".//span[text()=' Удалить']"))
+        )
+
+        delete_btn.click()
+
+
+
+        assert WebDriverWait(driver, 10).until(
+            EC.invisibility_of_element_located((By.XPATH, "//*[contains(text(), 'new autotask case №1')]"))
+        )
+
+    add_task_noproject_case1(driver)
+    driver.quit()
